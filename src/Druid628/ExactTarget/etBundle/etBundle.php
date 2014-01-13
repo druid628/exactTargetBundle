49a7e4d1201eab0dc33c8bae5b4edb33a6b99bc1
Micah Breedlove <druid628@gmail.com>
2014-01-12 22:30:11 -0600

    Initial Commit

---
+++ src/Druid628/ExactTarget/etBundle/etBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Druid628\ExactTarget\etBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class etBundle extends Bundle
+{
+    public function boot()
+    {
+    }
+
+}
